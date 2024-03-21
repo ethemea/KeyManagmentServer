0428f6117aef510d4cf5572fe9a0e313ee2ac60c
Anastasiya K <casde3661@gmail.com>
2024-03-21 23:36:12 +0700

    Add exposedHeaders

--- src/main/java/com/example/kms/controller/EmployeeController.java
+++ src/main/java/com/example/kms/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:8081", "http://muddled-event-production.up.railway.app"})
+@CrossOrigin(origins = "http://localhost:8081", exposedHeaders = "Access-Control-Allow-Origin")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")

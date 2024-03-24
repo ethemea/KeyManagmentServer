e0cd03a171b4c78d0a13cfe7965755e71b898a18
Anastasiya K <casde3661@gmail.com>
2024-03-25 00:28:27 +0700

    Fix Key Service

--- src/main/java/com/example/kms/controller/KeyController.java
+++ src/main/java/com/example/kms/controller/KeyController.java
@@ -2,6 +2,7 @@ package com.example.kms.controller;
 
 import com.example.kms.entity.Audience;
 import com.example.kms.entity.Key;
+import com.example.kms.entity.KeyState;
 import com.example.kms.form.AudienceForm;
 import com.example.kms.form.KeyForm;
 import com.example.kms.service.KeyService;
@@ -33,5 +34,10 @@ public class KeyController {
     @PostMapping("/keys")
     public ResponseEntity<Key> createKey(@RequestBody KeyForm form) {
         return new ResponseEntity<>(service.createKey(form), HttpStatus.CREATED);
+    }
+    @Operation(summary = "Update key", description = "Returns updated key data")
+    @PutMapping("/keys/{id}")
+    public ResponseEntity<Key> updateKey(@PathVariable("id") Integer id, KeyForm form) {
+        return new ResponseEntity<>(service.updateKey(id, form), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/kms/service/KeyService.java
+++ src/main/java/com/example/kms/service/KeyService.java
@@ -2,6 +2,7 @@ package com.example.kms.service;
 
 import com.example.kms.entity.Audience;
 import com.example.kms.entity.Key;
+import com.example.kms.entity.KeyState;
 import com.example.kms.form.KeyForm;
 import com.example.kms.repository.AudienceRepository;
 import com.example.kms.repository.KeyRepository;
@@ -24,5 +25,16 @@ public class KeyService {
 
     public List<Key> getAllKeys(){
         return keyRepository.findAll();
+    }
+
+    public Key updateKey(Integer id, KeyForm form){
+        Key key = keyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Not found user audience id = " + id));
+        Audience audience = audienceRepository.findById(form.getAudience_id())
+                .orElseThrow(() -> new RuntimeException("Not found user audience id = " + form.getAudience_id()));
+        key.setKeyState(form.getKeyState());
+        key.setAudience(audience);
+        key.setMain(form.isMain());
+        return keyRepository.save(key);
     }
 }
